958ccb3246d59a6317eb61f17decb98a9c1dc792
Rafael <rafgger@email.cz>
2025-04-16 17:00:20 +0200

    dfx canister id nft -> into App.jsx

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -8,14 +8,14 @@ import Minter from "./Minter";
 
 function App() {
 
-  const NFTID = "mxi2w-mirud-7hvs3-dvwpn-ssjti-df6qk-bt4l2-ywi3q-nfh5q-pisv6-jqe"; // dfx identity get-principal
+  const NFTID = "rrkah-fqaaa-aaaaa-aaaaq-cai"; // dfx identity get-principal
 
   return (
     <div className="App">
       <Header />
-      <Item />
+      {/* <Item /> */}
       {/* <Minter /> */}
-      {/* <Item id={NFTID} /> */}
+      <Item id={NFTID} />
       {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
     </div>
--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
+import { Actor, HttpAgent } from "@dfinity/agent";
+import { idlFactory } from "../../../declarations/nft"
+// import { canisterId } from "../../../declarations/nft/index";
+import { Principal } from "@dfinity/principal";
 
-function Item() {
+function Item(props) {
+
+  const [name, setName] = useState();
+
+  const id = Principal.fromText(props.id);
+
+  const localHost = "http://localhost:8080/";
+  const agent = new HttpAgent({ host: localHost });
+
+  async function loadNFT() {
+    const NFTActor = await Actor.createActor(idlFactory, {
+      agent,
+      canisterId: id
+    });
+    const name = await NFTActor.getName();
+    setName(name);
+  };
+
+  useEffect(() => {
+    loadNFT();
+  }, []);
+
   return (
     <div className="disGrid-item">
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
@@ -11,7 +36,7 @@ function Item() {
         />
         <div className="disCardContent-root">
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            CryptoDunks #312<span className="purple-text"></span>
+            {name}<span className="purple-text"></span>
           </h2>
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
             Owner: sdfsdf-erwerv-sdf
