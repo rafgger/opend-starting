0037e98c5124dbf1cb65b2b45a4104da652b256f
Rafael <rafgger@email.cz>
2025-04-16 16:24:58 +0200

    working-stage

--- src/opend_assets/src/components/App.jsx
+++ src/opend_assets/src/components/App.jsx
@@ -3,12 +3,19 @@ import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import homeImage from "../../assets/home-img.png";
+import Item from "./Item";
+import Minter from "./Minter";
 
 function App() {
+
+  const NFTID = "mxi2w-mirud-7hvs3-dvwpn-ssjti-df6qk-bt4l2-ywi3q-nfh5q-pisv6-jqe"; // dfx identity get-principal
+
   return (
     <div className="App">
       <Header />
-      <img className="bottom-space" src={homeImage} />
+      <Minter />
+      {/* <Item id={NFTID} /> */}
+      {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
     </div>
   );
